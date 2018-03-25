e971583cf433b4aa86eb715391c2828e5fa96d66
yangkai <csyang79@gmail.com>
2018-03-25 22:35:29 +0800

    update 326

--- leetcode-algorithms/326. Power of Three/326.Power_Of_Three.cpp
+++ leetcode-algorithms/326. Power of Three/326.Power_Of_Three.cpp
@@ -4,3 +4,14 @@ public:
         return (n != 0 && (n == 1 || (n % 3 == 0 && isPowerOfThree(n / 3))));
     }
 };
+/*
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+	if (n > 1)
+		while (n % 3 == 0)
+			n /= 3;
+	return n == 1;
+    }
+};
+*/
