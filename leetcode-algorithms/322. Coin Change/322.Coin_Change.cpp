8761e4e413e794cf5e7b5a0a13a32303373eac51
yangkai <csyang79@gmail.com>
2018-03-25 22:14:53 +0800

    add 322

---
+++ leetcode-algorithms/322. Coin Change/322.Coin_Change.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+        for (int i = 0; i < coins.size(); ++i)
+            for (int j = 1; j <= amount; ++j)
+                if (j - coins[i] >= 0)
+                    dp[j] = std::min(dp[j], dp[j - coins[i]] + 1);
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+};
